e9f16c29c5a64769a5eb41de9d3b25eca25bbaa9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 13:23:50 -0500

    Added a condition to display the winner's message

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import displayGameboard from '../modules/gameboard.js';
 import { selectPlayer } from '../modules/selectPlayer.js';
 import { selectOptionO, selectOptionX } from '../modules/selectOption.js';
 import game from '../modules/game.js';
+import displayWinner from '../modules/winner.js';
+import { players } from '../modules/players.js';
 
 const chooseX = document.getElementById('x');
 const chooseO = document.getElementById('O');
-
+const winner = document.querySelector('#div-winner h5');
 // Ads the default player 1 to the span.
 selectPlayer();
 
@@ -22,3 +24,12 @@ chooseO.addEventListener('click', selectOptionO);
 
 // To each div assign the selected choice.
 divs.forEach((div) => div.addEventListener('click', game));
+
+let player1X = players.player1.X.length;
+let player1O = players.player1.O.length;
+let player2X = players.player2.X.length;
+let player2O = players.player2.O.length;
+
+if ((player1X || player1O) || (player2X || player2O) >= 3) {
+  winner.textContent = displayWinner();
+}
