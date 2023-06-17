540adb5fa09191599272e80769041c51c7022ecb
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 15:20:41 -0500

    Modified a typo error and added a interval to verify the winner

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import game from '../modules/game.js';
 import displayWinner from '../modules/winner.js';
 import { players } from '../modules/players.js';
 
+//require('../modules/selectOption.js');
+
 const chooseX = document.getElementById('x');
-const chooseO = document.getElementById('O');
+const chooseO = document.getElementById('o');
 const winner = document.querySelector('#div-winner h5');
 // Ads the default player 1 to the span.
 selectPlayer();
@@ -25,11 +27,13 @@ chooseO.addEventListener('click', selectOptionO);
 // To each div assign the selected choice.
 divs.forEach((div) => div.addEventListener('click', game));
 
-let player1X = players.player1.X.length;
-let player1O = players.player1.O.length;
-let player2X = players.player2.X.length;
-let player2O = players.player2.O.length;
+setInterval(() => {
+  let player1X = players.player1.X.length;
+  let player1O = players.player1.O.length;
+  let player2X = players.player2.X.length;
+  let player2O = players.player2.O.length;
 
-if ((player1X || player1O) || (player2X || player2O) >= 3) {
-  winner.textContent = displayWinner();
-}
+  if ((player1X || player1O) >= 3 || (player2X || player2O) >= 3) {
+    winner.textContent = displayWinner();
+  }
+}, 1000);
