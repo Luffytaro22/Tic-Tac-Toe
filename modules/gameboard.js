cf55e0cd38988058bf56d1777bb830e679005e9b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 11:17:20 -0500

    Added a function to export the display

--- modules/gameboard.js
+++ modules/gameboard.js
@@ -1,9 +1,12 @@
 const container = document.getElementById('gameboard-container');
 
-for (let i = 1; i <= 3; i += 1) {
-  for (let j = 1; j <= 3; j += 1) {
-    const div = document.createElement('div');
-    div.classList.add('celds');
-    div.id = `[${i}, ${j}]`;
+export default function displayGameboard() {
+  for (let i = 1; i <= 3; i += 1) {
+    for (let j = 1; j <= 3; j += 1) {
+      const div = document.createElement('div');
+      div.classList.add('celds');
+      div.id = `[${i}, ${j}]`;
+      container.appendChild(div);
+    }
   }
 }
