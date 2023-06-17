4f5e1c7700f631d96c270b2b519cad67cd42e45d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 15:30:37 -0500

    Added the event listeners for the divs when the user selects their X or O choice

--- modules/selectOption.js
+++ modules/selectOption.js
@@ -1,10 +1,15 @@
-import { players } from '../modules/players.js';
+import { players } from './players';
+import game from './game.js';
 
 function selectOptionX() {
   const seleContainer = document.getElementById('selection-container');
   players.player1.choice = 'X';
   players.player2.choice = 'O';
   seleContainer.classList.add('hide');
+  // Select the created divs.
+  const divs = document.querySelectorAll('.celds');
+  // To each div assign the selected choice.
+  divs.forEach((div) => div.addEventListener('click', game));
 }
 
 function selectOptionO() {
@@ -12,6 +17,10 @@ function selectOptionO() {
   players.player1.choice = 'O';
   players.player2.choice = 'X';
   seleContainer.classList.add('hide');
+  // Select the created divs.
+  const divs = document.querySelectorAll('.celds');
+  // To each div assign the selected choice.
+  divs.forEach((div) => div.addEventListener('click', game));
 }
 
 export { selectOptionO, selectOptionX };
