2ca99072c7560a81463fca396b6b32024e6729f0
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 15:17:37 -0500

    Added a choice property

--- modules/players.js
+++ modules/players.js
@@ -1,10 +1,14 @@
-export let players = {
+let players = {
   player1: {
     X: [],
     O: [],
+    choice: '',
   },
   player2: {
     X: [],
     O: [],
+    choice: '',
   },
 };
+
+export { players };
