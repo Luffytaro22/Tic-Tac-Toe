a50b09acda212a9caf8044968dea2f226c9c5101
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 11:56:13 -0500

    Changed the players from a number to a string

--- modules/selectPlayer.js
+++ modules/selectPlayer.js
@@ -2,14 +2,14 @@ const span = document.getElementById('player-number');
 let player;
 function selectPlayer() {
   if (span.textContent === '') {
-    player = 1;
+    player = 'player1';
     span.textcontent = '1';
   } else {
     if (span.textcontent === '1') {
-      player = 2;
+      player = 'player2';
       span.textcontent = '2';
     } else {
-      player = 1;
+      player = 'player1';
       span.textContent = '1';
     }
   }
