113fc2bde646b196942f886267bd0795d3d5360e
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-18 09:41:55 -0500

    Module to add a new player based on their input

---
+++ modules/addPlayer.js
@@ -0,0 +1,18 @@
+export default function addPlayer() {
+  // Create elements.
+  const divPlayers = document.getElementById('players');
+  const p = document.createElement('p');
+  const container = document.createElement('div');
+  const span = document.createElement('span');
+  const inputNick = document.getElementById('nickname');
+
+  // Add classes
+  container.classList.add('players');
+  span.classList.add('score');
+
+  // Append elements.
+  p.textContent = inputNick.value;
+  container.appendChild(p);
+  container.appendChild(span);
+  divPlayers.appendChild(container);
+}
