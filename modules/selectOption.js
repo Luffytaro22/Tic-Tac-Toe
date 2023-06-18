47e329d64d8692c74a2f94e9f9fff8537bf3faca
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-17 20:34:10 -0500

    Evaluate the event.target.id to reduce code

--- modules/selectOption.js
+++ modules/selectOption.js
@@ -1,10 +1,15 @@
 import { players } from './players.js';
 import game from './game.js';
 
-function selectOptionX() {
+function selectOption(event) {
   const seleContainer = document.getElementById('selection-container');
-  players.player1.choice = 'X';
-  players.player2.choice = 'O';
+  if (event.target.id === 'x') {
+    players.player1.choice = 'X';
+    players.player2.choice = 'O';
+  } else {
+    players.player1.choice = 'O';
+    players.player2.choice = 'X';
+  }
   seleContainer.classList.add('hide');
   // Select the created divs.
   const divs = document.querySelectorAll('.celds');
@@ -12,15 +17,4 @@ function selectOptionX() {
   divs.forEach((div) => div.addEventListener('click', game));
 }
 
-function selectOptionO() {
-  const seleContainer = document.getElementById('selection-container');
-  players.player1.choice = 'O';
-  players.player2.choice = 'X';
-  seleContainer.classList.add('hide');
-  // Select the created divs.
-  const divs = document.querySelectorAll('.celds');
-  // To each div assign the selected choice.
-  divs.forEach((div) => div.addEventListener('click', game));
-}
-
-export { selectOptionO, selectOptionX };
+export { selectOption };
